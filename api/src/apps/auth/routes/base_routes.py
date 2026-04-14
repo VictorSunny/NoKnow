26d14d0d2a502c5c5ea80d11e0d3f7e7d3df41db
Victor Sunny <victorsunny432@gmail.com>
2026-04-14 15:18:34 +0100

    backend - minor editing on auth route file imports

--- api/src/apps/auth/routes/base_routes.py
+++ api/src/apps/auth/routes/base_routes.py
@@ -19,7 +19,7 @@ from src.apps.user.schemas.base_schemas import UserComplete
 from src.apps.auth.services.verification_services import check_user_data_is_acceptable
 from src.generics.schemas import ConfirmationText, IsValidResponse, MessageResponse
 from src.configurations.limiter import api_limiter
-from src.generics.services import password_check_response
+from src.generics.validation_services import password_check_response
 from src.apps.auth.services.otp_services import (
     confirm_otp_code_create_otp_jwt,
     create_send_cache_otp,
