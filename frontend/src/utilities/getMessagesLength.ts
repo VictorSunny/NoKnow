28438a736025eafe05fe11fa1cc96f1886f31223
Victor Sunny <victorsunny432@gmail.com>
2026-04-14 22:08:19 +0100

    frontend general commit for project structure changes

--- frontend/src/utilities/getMessagesLength.ts
+++
@@ -1,9 +0,0 @@
-import { MessageListResponse } from "../schemas/ChatSchemas";
-
-export default function getMessagesLength(messagesPages: MessageListResponse[]): number {
-  let total = 0;
-  messagesPages.map((page) => {
-    total += page.messages.length;
-  });
-  return total;
-}
