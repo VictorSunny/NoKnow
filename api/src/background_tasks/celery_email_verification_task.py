dfb0287765ac49950d89ed298292b32c8448fd0d
Victor Sunny <victorsunny432@gmail.com>
2026-04-14 21:23:52 +0100

    backend - minor renaming

--- api/src/background_tasks/celery_email_verification_task.py
+++ api/src/background_tasks/celery_email_verification_task.py
@@ -20,8 +20,8 @@ if system_is_windows:
 
 logger = getLogger(__name__)
 
-celery_app = Celery()
-celery_app.config_from_object("src.configurations.config")
+celery_email_verification_app = Celery()
+celery_email_verification_app.config_from_object("src.configurations.config")
 
 r_client = Redis.from_url(
     url=Config.REDIS_URL,
@@ -33,11 +33,11 @@ r_client = Redis.from_url(
     ),
 )
 if system_is_windows:
-    celery_app.conf.update(
+    celery_email_verification_app.conf.update(
         worker_pool="solo", task_track_started=True, timezone="UTC", enable_utc=True
     )
 
-@celery_app.task(
+@celery_email_verification_app.task(
     bind=True,
     max_retries=2,
     default_retry_delay=8,
