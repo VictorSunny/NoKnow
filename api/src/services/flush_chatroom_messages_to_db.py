a61a5f064d1c89762fd81312736b14fe868c207f
Victor Sunny <victorsunny432@gmail.com>
2026-04-18 16:14:43 +0100

    backend - implemented graceful shutdown of loops. some refactoring

--- api/src/services/flush_chatroom_messages_to_db.py
+++ api/src/services/flush_chatroom_messages_to_db.py
@@ -2,7 +2,8 @@ import ast
 import asyncio
 from logging import getLogger
 
-from fastapi import FastAPI
+import redis.asyncio as redis
+
 from src.db.models import Message
 from src.db.database import async_session_maker
 
@@ -11,21 +12,27 @@ from src.configurations.config import Config
 
 logger = getLogger(__name__)
 
-
-async def flush_messages_to_db(app: FastAPI):
-    r_client = app.state.r_client
-    while True:
-        queued_messages_length = await r_client.llen(Config.REDIS_MESSAGE_LIST)
-        if (queued_messages_length) and (int(queued_messages_length) > 0):
-            message_batch = await r_client.lpop(Config.REDIS_MESSAGE_LIST, 200)
-            logger.info(
-                f"flushing {queued_messages_length} messages from redis to database"
-            )
-            async with async_session_maker() as db:
-                for message in message_batch:
-                    parsed_message = ast.literal_eval(message)
-                    new_message = Message(**parsed_message)
-                    db.add(new_message)
-                await db.commit()
-                await db.close()
-        await asyncio.sleep(0.3)
+# async def flush_messages_to_db(app: FastAPI):
+async def flush_messages_to_db(r_client: redis.Redis):
+    try:
+        await r_client.ping()
+        logger.info("successfully started periodic chat messages flusher")
+        while True:
+            queued_messages_length = await r_client.llen(Config.REDIS_MESSAGE_LIST)
+            if (queued_messages_length) and (int(queued_messages_length) > 0):
+                message_batch = await r_client.lpop(Config.REDIS_MESSAGE_LIST, 200)
+                logger.info(
+                    f"flushing {queued_messages_length} messages from redis to database"
+                )
+                async with async_session_maker() as db:
+                    for message in message_batch:
+                        parsed_message = ast.literal_eval(message)
+                        new_message = Message(**parsed_message)
+                        db.add(new_message)
+                    await db.commit()
+                    await db.close()
+            print("done messages loop")
+            await asyncio.sleep(4)
+    except asyncio.CancelledError:
+        logger.info("stopped periodic chat messages flusher")
+        raise        
--- api/src/services/flush_chatroom_modification_timestamps_to_db.py
+++ api/src/services/flush_chatroom_modification_timestamps_to_db.py
@@ -1,4 +1,8 @@
 import asyncio
+from concurrent.futures import CancelledError
+
+import redis.asyncio as redis
+
 from logging import getLogger
 from uuid import UUID
 
@@ -13,29 +17,38 @@ from src.configurations.config import Config
 
 logger = getLogger(__name__)
 
-
-async def flush_chatroom_modification_timestamps_to_db(app: FastAPI):
-    r_client = app.state.r_client
-    while True:
-        queued_modification_timestamps = await r_client.hgetall(
-            Config.REDIS_CHATROOM_MODIFICATION_DATE_LIST_NAME
-        )
-        queue_length = len(queued_modification_timestamps)
-        if queue_length > 0:
-            logger.info(
-                f"flushing {queue_length} modification_timestamps from redis to database"
+# async def flush_chatroom_modification_timestamps_to_db(app: FastAPI):
+async def flush_chatroom_modification_timestamps_to_db(r_client: redis.Redis):
+    # r_client = app.state.r_client
+    try:
+        await r_client.ping()
+        logger.info("successfully started periodic chat modificcation time flusher")
+        while True:
+            queued_modification_timestamps = await r_client.hgetall(
+                Config.REDIS_CHATROOM_MODIFICATION_DATE_LIST_NAME
             )
-            async with async_session_maker() as db:
-                for key, value in queued_modification_timestamps.items():
-                    query = (
-                        update(Chatroom)
-                        .where(Chatroom.uid == UUID(key))
-                        .values(modified_at=float(value))
-                    )
-                    await r_client.hdel(
-                        Config.REDIS_CHATROOM_MODIFICATION_DATE_LIST_NAME, key
-                    )
-                    await db.execute(query)
-                await db.commit()
-                await db.close()
-        await asyncio.sleep(60)
+            queue_length = len(queued_modification_timestamps)
+            if queue_length > 0:
+                logger.info(
+                    f"flushing {queue_length} modification_timestamps from redis to database"
+                )
+                async with async_session_maker() as db:
+                    for key, value in queued_modification_timestamps.items():
+                        query = (
+                            update(Chatroom)
+                            .where(Chatroom.uid == UUID(key))
+                            .values(modified_at=float(value))
+                        )
+                        await r_client.hdel(
+                            Config.REDIS_CHATROOM_MODIFICATION_DATE_LIST_NAME, key
+                        )
+                        await db.execute(query)
+                    await db.commit()
+                    await db.close()
+            print("done timestamp loop")
+            await asyncio.sleep(60)
+    except asyncio.CancelledError:
+        logger.info("stopped periodic chat modification time flusher")
+        raise
+    except:
+        raise
