5d6b36235954f1916c8714eb7753cf148e6b698a
Victor Sunny <victorsunny432@gmail.com>
2026-04-19 21:25:47 +0100

    backend - created multiple user caching services

--- api/src/caching/services/redis_user_caching.py
+++ api/src/caching/services/redis_user_caching.py
@@ -1,21 +1,52 @@
-# import json
-# from logging import getLogger
-# from uuid import UUID
-# import redis.asyncio as redis
-# from src.apps.auth.schemas.base_schemas import UserCache
-# from src.configurations.config import Config
-# from src.db.models import User
+import json
+from logging import getLogger
+from uuid import UUID
+import redis.asyncio as redis
+from src.caching.schemas.user_cache_schemas import UserCache
+from src.configurations.config import Config
+from src.db.models import User
 
-# logger = getLogger(__name__)
+logger = getLogger(__name__)
 
-# async def set_user_cache(user: User, r_client: redis.Redis):
-#     user_data = json.loads(user.model_dump_json(exclude_none=True))
-#     await  r_client.hset(name=Config.REDIS_USER_NAME_PREFIX, key=str(user.uid), mapping=user_data)
-#     logger.info(f"successfully set cache for user with id: {user.uid}")
+async def clear_user_cache(user: User, r_client: redis.Redis):
+    uid_key = f"{Config.REDIS_USER_NAME_PREFIX}:{str(user.uid)}"
+    email_key = f"{Config.REDIS_USER_NAME_PREFIX}:{user.email}"
+    username_key = f"{Config.REDIS_USER_NAME_PREFIX}:{user.username}"
 
-# async def get_user_from_cache(id: UUID, r_client: redis.Redis):
-#     user_cache = None
-#     redis_res = await r_client.hgetall(Config.REDIS_USER_NAME_PREFIX, str(id))
-#     if redis_res:
-#         user_cache = UserCache(**redis_res)
-#         logger.info(f"successfully retrieved cache for user with id: {id}")
+    await r_client.delete(uid_key)
+    await r_client.delete(email_key)
+    await r_client.delete(username_key)
+
+async def set_user_cache(user: User, r_client: redis.Redis):
+    await clear_user_cache(user=user, r_client=r_client)
+    
+    print("creating cache for user", user.uid)
+    
+    user_data = json.loads(user.model_dump_json(exclude_none=True))
+    user_data = {key: str(value) for key, value in user_data.items()}
+    
+    for key, value in user_data.items():
+        print(key, type(value))
+
+    uid_key = f"{Config.REDIS_USER_NAME_PREFIX}:{str(user.uid)}"
+    email_key = f"{Config.REDIS_USER_NAME_PREFIX}:{user.email}"
+    username_key = f"{Config.REDIS_USER_NAME_PREFIX}:{user.username}"
+    
+    await  r_client.hset(uid_key, mapping=user_data)
+    await  r_client.hset(username_key, mapping=user_data)
+    await  r_client.hset(email_key, mapping=user_data)
+    
+    logger.info(f"successfully set cache for user with id: {user.uid}")
+
+async def get_user_from_cache(id: UUID | str, r_client: redis.Redis):
+    user_cache = None
+    redis_res = await r_client.hgetall(f"{Config.REDIS_USER_NAME_PREFIX}:{str(id)}")
+    if redis_res:
+        print("found cache for user with id", id)
+        user_cache = UserCache(**redis_res)
+        print(user_cache.model_dump_json())
+        logger.info(f"successfully retrieved cache for user with identifier: {id}")
+    else:
+        print("no cache for user with id", id)
+    return user_cache
+
